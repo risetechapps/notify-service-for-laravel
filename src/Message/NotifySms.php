ae60fb37c9f8112636db91f83f8a9d9ba3ec30f9
Rise Tech <apps@risetech.com.br>
2025-12-29 15:03:39 -0300

    Criado classe para gerneciar o envio de sms

---
+++ src/Message/NotifySms.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RiseTechApps\Notify\Message;
+
+class NotifySms
+{
+    protected string $content = "";
+    protected string $to = "";
+    protected string $from = "";
+
+    public function content(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function to(string $to): static
+    {
+        if (blank($this->to)) {
+            $this->to = $to;
+        }
+        return $this;
+    }
+
+    public function from(string $from): static
+    {
+        if (blank($this->from)) {
+            $this->from = $from;
+        }
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'content' => $this->content,
+            'to' => $this->to,
+            'from' => $this->from,
+        ];
+    }
+}
