10e8ffdfec6ac0e464d68f611f3345499bc1cfcb
risetechapps <apps@risetech.com.br>
2026-03-11 18:35:41 -0300

    Atualizado Channel

---
+++ src/Channel/NotifyChannelPush.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace RiseTechApps\Notify\Channel;
+
+use Exception;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Http;
+use RiseTechApps\Notify\Events\NotifyFailedEvent;
+use RiseTechApps\Notify\Events\NotifySendingEvent;
+use RiseTechApps\Notify\Events\NotifySentEvent;
+use RiseTechApps\Notify\Message\NotifyPush;
+
+class NotifyChannelPush extends NotifyChannel
+{
+    /**
+     * @throws Exception
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        try {
+            $message = $notification->toNotifyPush($notifiable);
+
+            if (!$message instanceof NotifyPush) {
+                return null;
+            }
+
+            if (!$message->token && $to = $notifiable->routeNotificationFor('push', $notification)) {
+                $message->token($to);
+            }
+
+            Event::dispatch(new NotifySendingEvent($notifiable, $notification, 'push'));
+
+            $response = Http::withHeaders([
+                'X-API-KEY' => $this->apiKey,
+            ])
+                ->acceptJson()
+                ->post("{$this->apiUrl}/api/v1/send/push", $message->toArray());
+
+            if ($response->failed()) {
+                throw new Exception('Error sending notification: ' . $response->body());
+            }
+
+            $responseJson = $response->json();
+
+            Event::dispatch(new NotifySentEvent($notifiable, $notification, $responseJson, 'push'));
+
+            logglyInfo()->performedOn(self::class)
+                ->withProperties(['notifiable' => $notifiable, 'notification' => $notification, 'response' => $responseJson])->log("Notification sent");
+
+            return $responseJson;
+        } catch (\Exception $exception) {
+            Event::dispatch(new NotifyFailedEvent($notifiable, $notification, $exception, 'push'));
+
+            logglyError()
+                ->withProperties(['notifiable' => $notifiable, 'notification' => $notification])
+                ->exception($exception)->log("Error by sending notification");
+
+            report($exception);
+
+            return null;
+        }
+    }
+}
